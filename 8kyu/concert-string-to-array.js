a60fd965061973b414195e6052bfbaae68b77017
notanothertaylor <taylorstechspace@gmail.com>
2022-07-28 23:34:42 -0400

    adding codewars from today

---
+++ 8kyu/concert-string-to-array.js
@@ -0,0 +1,10 @@
+// DESCRIPTION:
+// Write a function to split a string and convert it into an array of words.
+
+// My solution
+function stringToArray(string){
+    return string.split(' ')
+  }
+
+//   Other solutions
+// const stringToArray = string => string.split(' ');
